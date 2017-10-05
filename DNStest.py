f73dc8c24b6906b1be18e282334dcc71c04d3d48
Ryan Kavanaugh <ryan.kavanaugh@crc-corp.com>
2017-10-05 09:35:41 -0700

    Adding DNS test script written in python. Please make sure to add the required libraries. Enjoy!

---
+++ DNStest.py
@@ -0,0 +1,72 @@
+# /Users/ryankavanaugh/Desktop/DNS\ Test
+import socket
+import xlrd
+import requests
+import urllib2
+import json
+import unittest
+
+workbook = xlrd.open_workbook('DNS Testing.xlsx')
+worksheet = workbook.sheet_by_index(0)
+
+
+class Verify_DNS_Data_From_Spreadsheet(unittest.TestCase):
+    def test_dns_data(self):
+        testCounter = 0
+        print
+        # Runs through the Domain Names & Verifies the IP addresses
+        print 'Domain Name vs. NS Look Up Address:'
+        for x in range(1, 55):
+
+            try:
+                domainName = worksheet.cell(x, 0).value
+                ipAllNumbers = socket.gethostbyname(domainName)
+                ipEnd = ipAllNumbers[-3:]
+                ipEnd = int(ipEnd)
+                nsLookUpAddress = int(worksheet.cell(x, 3).value)
+
+                if (ipEnd != nsLookUpAddress):
+                    print domainName + ' is providing an incorrect nslookup address'
+                    testCounter += 1
+
+            except:
+                print 'Row number: ' + str(x + 1)
+                print domainName + ' did not connect properly'
+                testCounter += 1
+
+        print
+
+        # Run through the CARGTM Names and ensure they respond correctly from a curl-style request
+        print 'CARGTM Name vs. Expected Response:'
+        for x in range(1, 55):
+            try:
+                item = str(worksheet.cell(x, 1).value)
+                url = 'http://' + item
+
+                res = requests.post(url)
+
+                req = urllib2.Request(url)
+                response = urllib2.urlopen(req)
+                result = response.read()
+
+                expectedResponse = str(worksheet.cell(x, 2).value)
+
+                if expectedResponse not in result:
+                    print url
+                    print expectedResponse
+                    print 'Row number: ' + str(x + 1)
+                    print
+                    testCounter += 1
+            except:
+                print str(worksheet.cell(x, 1).value)
+                print 'Did no connect properly'
+                print 'Row number: ' + str(x + 1)
+                print
+                testCounter += 1
+
+        if testCounter > 0:
+            assert False, 'DNS test failed'
+
+
+if __name__ == '__main__':
+    unittest.main()
