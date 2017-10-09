90a7fb6a372c98fe446130b5b7f2afa983378b0a
Ryan Kavanaugh <ryan.kavanaugh@crc-corp.com>
2017-10-09 10:58:24 -0700

    second while loop

--- dns.py
+++ dns.py
@@ -13,47 +13,48 @@ worksheet = workbook.sheet_by_index(0)
 class Verify_DNS_Data_From_Spreadsheet(unittest.TestCase):
     def test_dns_data(self):
         testCounter = 0
-        domainName = ''
+        ipAllNumbers = None
         print
         # Runs through the Domain Names & Verifies the IP addresses
         print 'Domain Name vs. NS Look Up Address:'
         for x in range(1, 55):
 
-            while domainName == None:
+            while ipAllNumbers == None:
                 try:
                     domainName = worksheet.cell(x, 0).value
                     ipAllNumbers = socket.gethostbyname(domainName)
                     ipEnd = ipAllNumbers[-3:]
                     ipEnd = int(ipEnd)
                     nsLookUpAddress = int(worksheet.cell(x, 3).value)
-
                     if (ipEnd != nsLookUpAddress):
                         print domainName + ' is providing an incorrect nslookup address'
                         testCounter += 1
 
                 except:
-                    print domainName
-                    print 'here'
+                    print ipAllNumbers
+                    print 'Check the ip for a discrepancy'
                     print 'Row number: ' + str(x + 1)
                     print domainName + ' did not connect properly'
                     testCounter += 1
 
-            domainName = ''
+            ipAllNumbers = None
 
         print
 
         # Run through the CARGTM Names and ensure they respond correctly from a curl-style request
         print 'CARGTM Name vs. Expected Response:'
+
         for x in range(1, 55):
             try:
                 item = str(worksheet.cell(x, 1).value)
                 url = 'http://' + item
+                result = None
 
-                res = requests.post(url)
-
-                req = urllib2.Request(url)
-                response = urllib2.urlopen(req)
-                result = response.read()
+                while result == None:
+                    res = requests.post(url)
+                    req = urllib2.Request(url)
+                    response = urllib2.urlopen(req)
+                    result = response.read()
 
                 expectedResponse = str(worksheet.cell(x, 2).value)
 
@@ -69,6 +70,7 @@ class Verify_DNS_Data_From_Spreadsheet(unittest.TestCase):
                 print 'Row number: ' + str(x + 1)
                 print
                 testCounter += 1
+
 
         if testCounter > 0:
             assert False, 'DNS test failed'
